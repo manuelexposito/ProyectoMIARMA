5049b42648942c023251766a93a80783fc2f42e4
ManuelExposito <manuelexposito4d@gmail.com>
2022-03-07 12:09:02 +0100

    cambios

--- miarma/src/main/java/com/salesianostriana/miarma/controllers/AuthController.java
+++ miarma/src/main/java/com/salesianostriana/miarma/controllers/AuthController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@CrossOrigin("http://localhost:4200/")
+@CrossOrigin(origins = "http://localhost:4200/")
 public class AuthController {
 
     private final AuthenticationManager authenticationManager;
